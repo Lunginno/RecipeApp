618f77f92437195f20608c5cd8b0b4857c2dffef
Lunginno <sifumbalungile@gmail.com>
2024-06-07 15:00:57 +0200

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { RegisterpageComponent } from './registerpage/registerpage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DetailspageComponent } from './detailspage/detailspage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginpageComponent,
+    RegisterpageComponent,
+    NavbarComponent,
+    DetailspageComponent
   ],
   imports: [
     BrowserModule,
