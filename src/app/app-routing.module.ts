36043e83025192d23e4f13384d135b7cc10e98b0
Lunginno <sifumbalungile@gmail.com>
2024-07-08 16:22:54 +0200

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginpageComponent } from './pages/loginpage/loginpage.component';
+import { RegisterpageComponent } from './pages/registerpage/registerpage.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LandingpageComponent } from './pages/landingpage/landingpage.component';
+const routes: Routes = [
+  { path: 'login', component: LoginpageComponent},
+  { path: 'register', component: RegisterpageComponent},
+  { path: 'home', component: HomepageComponent},
+  { path: 'landing', component: LandingpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
