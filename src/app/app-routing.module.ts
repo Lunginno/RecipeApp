8fd5b9ff12fcf0ae2598f75822881b009bc66840
Lunginno <sifumbalungile@gmail.com>
2024-07-12 16:17:40 +0200

    details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginpageComponent } from './pages/loginpage/loginpage.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { LandingpageComponent } from './pages/landingpage/landingpage.component';
+import { DetailspageComponent } from './pages/detailspage/detailspage.component';
 const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full' }, // Redirect root path to /landing
   { path: 'login', component: LoginpageComponent},
   { path: 'home', component: HomepageComponent},
-  { path: 'landing', component: LandingpageComponent}
+  { path: 'landing', component: LandingpageComponent},
+  { path: 'details', component: DetailspageComponent}
 ];
 
 @NgModule({
