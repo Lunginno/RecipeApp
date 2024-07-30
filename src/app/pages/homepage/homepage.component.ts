cff92e450d8a22727329c615823d897c346ff0a1
Lunginno <sifumbalungile@gmail.com>
2024-07-30 16:21:17 +0200

    details & homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoryService } from 'src/app/services/category/category.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit{
+  category!: string;
+  meals: any[] = [];
 
+  constructor(
+    private categoryservice: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.category = params.get('category') || '';
+      this.fetchMeals();
+    });
+  }
+  fetchMeals(): void {
+        if (this.category) {
+          this.categoryservice.getMealsByCategory(this.category).subscribe(data => {
+            this.meals = data.meals;
+          });
+        }
+      }
 }
